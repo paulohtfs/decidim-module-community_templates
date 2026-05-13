4a92c75d9108b0274a48b4cbf5a5aab9b7e91e3b
Paulo Tada <paulohtfs@gmail.com>
2026-05-13 14:52:31 -0300

    fix: changing Webpacker to Shakapacker

--- config/assets.rb
+++ config/assets.rb
@@ -2,8 +2,8 @@
 
 base_path = File.expand_path("..", __dir__)
 
-Decidim::Webpacker.register_path("#{base_path}/app/packs")
-Decidim::Webpacker.register_entrypoints(
+Decidim::Shakapacker.register_path("#{base_path}/app/packs")
+Decidim::Shakapacker.register_entrypoints(
   decidim_admin_community_templates: "#{base_path}/app/packs/entrypoints/decidim_admin_community_templates.js"
 )
-Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/community_templates/admin/admin")
+Decidim::Shakapacker.register_stylesheet_import("stylesheets/decidim/community_templates/admin/admin")
